bf72efdf471c7548a5d0d6120cbee1233763c525
Mario Cruz <marioalfonsocruz9@gmail.com>
2026-07-10 13:28:08 -0600

    Adding first configurations

---
+++ src/main/java/com/coworking/CoworkingReservationApiApplication.java
@@ -0,0 +1,13 @@
+package com.coworking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoworkingReservationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoworkingReservationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coworking/CoworkingReservationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coworking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoworkingReservationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
